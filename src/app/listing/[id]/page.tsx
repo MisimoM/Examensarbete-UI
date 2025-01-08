f345f6059a4f46547219a9dc72e0acbf5c2cde1a
Marko Misimovic <misimovicmarko@gmail.com>
2025-01-08 09:30:57 +0100

    Add listing details page

--- src/app/listing/[id]/page.tsx
+++ src/app/listing/[id]/page.tsx
@@ -4,48 +4,112 @@ import Image from 'next/image';
 import house from "@/public/house.jpg";
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { Input } from '@/src/app/components/ui/input';
+import { ButtonOrLink } from '@/src/app/components/ui/button';
 
 export default function Listing() {
 
-    const params = useParams()
+  const params = useParams()
 
-    interface Listing {
-        title: string;
-        description: string;
-        subLocation: string;
-        mainLocation: string;
-        price: number;
-        accomodationType: string;
-        availableFrom: string;
-        availableUntil: string;
+  interface Listing {
+    title: string;
+    description: string;
+    subLocation: string;
+    mainLocation: string;
+    price: number;
+    accomodationType: string;
+    availableFrom: string;
+    availableUntil: string;
+  }
+  
+  const [listing, setListing] = useState<Listing>();
+  const [startDate, setStartDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
+  
+  useEffect(() => {
+    if (params && params.id) {
+      fetch(`https://localhost:7186/Listings/GetById/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => setListing(data))
+        .catch((err) => console.error('Error fetching listing:', err));
       }
+  }, [params]);
   
-    const [listing, setListing] = useState<Listing>();
-  
-    useEffect(() => {
-      if (params && params.id) {
-        // Hämta annonsen från API:et
-        fetch(`https://localhost:7186/Listings/GetById/${params.id}`)
-          .then((res) => res.json())
-          .then((data) => setListing(data))
-          .catch((err) => console.error('Error fetching listing:', err));
-      }
-    }, [params]);
-  
-    if (!listing) return <p>Loading...</p>;
+  if (!listing) return <p>Loading...</p>;
+
+  const pricePerNight = listing.price
     
-    return (
-        <main>
-            <div className="container mx-auto w-6/12 my-2">
-                <Image src={house} alt="House" />
-                <h2>{listing.title}</h2>
-                <p>{listing.description}</p>
-                <p>{listing.subLocation}, {listing.mainLocation}</p>
-                <p>{listing.price}kr natt</p>
-                <p>{listing.accomodationType}</p>
-                <p>{listing.availableFrom}</p>
-                <p>{listing.availableUntil}</p>
-            </div>
-        </main>
-    )
+  const calculateDays = () => {
+    if (!startDate || !endDate) return 0;
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const differenceInTime = end.getTime() - start.getTime();
+    
+    return differenceInTime > 0 ? differenceInTime / (1000 * 60 * 60 * 24) : 0;
+  };
+    
+  const totalPrice = calculateDays() * pricePerNight;
+
+  const userId = "88eece02-e933-450f-9b10-9e19fb9e9619";
+
+  const handleBooking = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!startDate || !endDate || !listing) return;
+
+    const bookingData = {
+      userId: userId,
+      listingId: params.id,
+      startDate: startDate,
+      endDate: endDate
+    };
+
+    fetch('https://localhost:7186/Bookings/Create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bookingData)
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert('Bokning genomförd!');
+        } else {
+          alert('Något gick fel vid bokningen.');
+        }
+      })
+      .catch((err) => {
+        console.error('Error creating booking:', err);
+        alert('Något gick fel vid bokningen.');
+      });
+  };
+    
+  return (
+    <main>
+      <div className="container flex gap-4 mx-auto w-6/12 my-2">
+        <div>
+          <Image className='rounded-md' src={house} alt="House" />
+          <h2>{listing.title}</h2>
+          <p>{listing.description}</p>
+          <p>{listing.subLocation}, {listing.mainLocation}</p>
+          <p>{listing.price}kr natt</p>
+          <p>{listing.accomodationType}</p>
+          <p>{listing.availableFrom}</p>
+          <p>{listing.availableUntil}</p>
+        </div>
+        <div>
+          <div className='p-3 border border-primary rounded'>
+            <form onSubmit={handleBooking} className='flex flex-col gap-3'>
+              <h3>Boka här</h3>
+              <div className='flex gap-2'>
+                <Input label='Startdatum' name='startDate' type='date' value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+                <Input label='Slutdatum' name='endDate' type='date' value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+              </div>
+              <h3>Totalt: {totalPrice}kr</h3>
+              <ButtonOrLink type='submit'>Boka</ButtonOrLink>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
